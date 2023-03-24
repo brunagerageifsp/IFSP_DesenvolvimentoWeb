c232aa41b28400989797a10b8c9b9450ebc0a209
Bruna Gerage <catena.bbarros@hubfintech.com.br>
2023-03-24 18:00:04 -0300

    Atualizando sistema-view

---
+++ sistema-view/src/components/MenuLateral.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-button v-b-toggle.menu-lateral>Abrir Menu</b-button>
+    <b-sidebar id="menu-lateral" title="Menu" shadow>
+        <b-nav vertical>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/cadastraproduto">Cadastro de Produtos</b-nav-item>
+            <b-nav-item to="/sobre">Sobre o Sistema</b-nav-item>
+        </b-nav>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ sistema-view/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/sobre',
+    name: 'sobre',   
+    component: () => import(/* webpackChunkName: "SobreView" */ '../views/SobreView.vue')
+  },
+  {
+    path: '/cadastraproduto',
+    name: 'cadastraproduto',
+    component: () => import(/* webpackChunkName: "CadastraProduto" */ '../views/CadastraProduto.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
