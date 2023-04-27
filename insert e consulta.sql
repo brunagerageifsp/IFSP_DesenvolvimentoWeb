2a917cf4f1bce89ebd000234fbbc2bbc87a52c6f
Bruna Gerage <catena.bbarros@hubfintech.com.br>
2023-04-27 19:21:32 -0300

    Subindo DER do banco e script da criacao do banco

--- insert e consulta.sql
+++
@@ -1,6 +0,0 @@
-insert into gestor values ('2','admin','123','daniel','123');
-
-select * from funcionario;
-select * from produto;
-
-insert into funcionario values ('10','Bruna','111','0119','abc');
